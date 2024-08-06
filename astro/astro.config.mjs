2720238cf71714a3c5de0c3fe84a5767bda4c448
Achyut Koirala <aoogleak@gmail.com>
2024-08-06 11:22:08 +0545

    Update astro.config.mjs

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -3,8 +3,8 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://axyut.github.io",
-    base: "/writeups",
+    site: "https://writeups.axyut.me",
+    base: "",
     integrations: [
         starlight({
             favicon: "./src/assets/logo.jpg",
